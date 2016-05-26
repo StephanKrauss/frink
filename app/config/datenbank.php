1f4239e02db7413a595e7d017e273882a6d79f13
Stephan Krauss <info@stephankrauss.de>
2016-05-26 06:34:00 +0200

    Konfiguration Datenbank in Git übernommen

---
+++ app/config/datenbank.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stephan.Krauss
+ * Date: 21.04.2016
+ * Time: 16:32
+ */
+
+$zugangswerte = array(
+    'hostname' => '127.0.0.1',
+    'database' => 'test',
+    'username' => 'root',
+    'password' => ''
+);
