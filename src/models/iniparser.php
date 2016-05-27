1798a24518060d59bc97b86c80d5984ffd193188
Stephan Krauss <info@stephankrauss.de>
2016-05-27 06:34:30 +0200

    Kommentare im INI Parser

--- src/models/iniparser.php
+++ src/models/iniparser.php
@@ -3,70 +3,50 @@
 namespace models;
 
 /**
- * Little Ini Parser
- *
- * LICENSE
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * @category Little
- * @package Little_Ini_Parser
- * @copyright Copyright (c) 2008-2020 Nikishaev Andrey (http://andreynikishaev.livejournal.com)
- * @license http://framework.zend.com/license/new-bsd New BSD License
- * @version 1.1
- */
+* Parsen der INI Dateien der zentralen Konfiguration
+*
+* @author Stephan Krauss
+* @copyright Stephan Krauss
+* @lisence MIT
+* @package Model
+* @date 27.05.2016
+*/
 
 class iniparser {
 
-        protected $declared = false;
+	protected $declared = false;
 
-        /**
-         * Loads configuration from the configuration files
-         *
-         * @param string $pathes - pathes to the configuration files
-         * @param string $sep_k - key separator string
-         * @param string $sep_v - value separator string
-         * @return array - parsed configuration the configuration files
-         */
+	/**
+	 * Übernimmt die Pfade der INI Dateien
+	 *
+	 * @param $pathes
+	 *
+	 * @return array
+	 */
+	public function parse($pathes)
+	{
+		$allConf = array();
 
-        public function parse($pathes)
-		{
-            $allConf = array();
+		foreach($pathes as $path){
+			$conf = $this->getArray($path);
+			$allConf= array_merge($allConf, $conf);
+		}
 
-            foreach($pathes as $path){
-                $conf = $this->getArray($path);
-				$allConf= array_merge($allConf, $conf);
-            }
+		return $allConf;
+	}
 
-            return $allConf;
-        }
+	/**
+	 * Zerlegen der INI Dateien in ein Array
+	 *
+	 * @param $path
+	 *
+	 * @return array
+	 */
+	protected function getArray($path)
+	{
+		$conf = parse_ini_file($path,true);
 
-        /**
-         * Loads configuration from the configuration file
-         * and returns the associative array
-         *
-         * @param string $path - path to the configuration file
-         * @param string $sep_k - key separator string
-         * @param string $sep_v - value separator string
-         * @return array - parsed configuration
-         */
+		return $conf;
 
-        protected function getArray($path)
-		{
-            $conf = parse_ini_file($path,true);
-
-            return $conf;
-
-        }
+	}
 }
