ad4104a466f2af1107e596914783437d096086eb
Stephan Krauss <develop@stephankrauss.de>
2016-06-26 10:22:04 +0200

    anlegen Controller Pattern mit Design Pattern Prototype master

---
+++ src/controller/creationalPattern1.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace controller;
+
+use tools\frinkError;
+
+/**
+ * Test der Erzeugungs Pattern unter Nutzung des DI Container Pimple
+ *
+ * + Prototype
+ *
+ * @author Stephan Krauß
+ * @copyright Stephan Krauss
+ * @lisence Stephan Krauß
+ * @date 22.06.2016
+ * @package controller
+ */
+class creationalPattern1 extends main
+{
+    /**
+     * erweitern Pimple um spezielle Model und Params
+     */
+    public function __construct($controllerName, $actionName)
+    {
+        // Vorbereitung des Pimple Container, Singleton Pattern
+        $models = [
+            'basis' => function ($pimple) {
+                return new \models\modelBasis($pimple);
+            },
+            'bla' => function ($pimple) {
+                return new \models\modelBla($pimple);
+            },
+            'blub' => function ($pimple) {
+                return new \models\modelBlub($pimple);
+            },
+            'modelSingletonBasis' => function ($pimple) {
+                return new \models\modelBasis($pimple);
+            },
+            'param1' => 'param1',
+            'param2' => 'param2'
+        ];
+
+        parent::pimple($models);
+
+        // Factory Pattern
+        $this->pimple['modelFactoryBasis'] = $this->pimple->factory(function ($pimple) {
+            return new \models\modelBasis($pimple);
+        });
+
+        parent::__construct($controllerName, $actionName);
+    }
+
+    /**
+     * Laden des Parent Template und Subtemplate des Baustein
+     */
+    protected function template()
+    {
+        try {
+            /** @var $session \models\session */
+            $modelSession = \Flight::get('session');
+
+            $outputTemplate = [
+                'masterTemplate' => 'main.html',
+                'templateSuperuser' => 'bla_superuser.html'
+            ];
+
+            $config = \Flight::get('config');
+
+            if ($config['debugBlock']['debug']) {
+                $outputTemplate['debugBlock'] = $this->setDebug($modelSession);
+            }
+
+            \Flight::view()->display($this->templateName, $outputTemplate);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Verwenden eines Standard Model aus dem DIC als Singleton.
+     *
+     * + Pimple verwendet jedes Objekt als Singleton
+     *
+     * @throws \Exception
+     * @throws frinkError
+     */
+    public function prototypePattern()
+    {
+        try {
+
+            $pimple = new \Pimple\Container();
+            $pimple['prototype'] = $this->pimple->factory(function ($pimple) {
+                return new \models\Car($pimple);
+            });
+            
+            
+            
+
+            // Übergabe an die View
+            $this->template();
+        } // eigene Exception
+        catch (\tools\frinkError $e) {
+            throw $e;
+        } // Exception anderer Klassen
+        catch (\Exception $e) {
+            throw $e;
+        }
+    }
+}
---
+++ src/models/Car.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace models;
+
+/**
+* Erstellen eines Car und deren Nutzung als Php Design Pattern
+*
+* @author Stephan Krauß
+* @copyright Stephan Krauss
+* @lisence Stephan Krauß
+* @date 26.06.2016
+*
+* @package models
+*/
+
+
+class Car extends \models\ModelData
+{
+
+}
