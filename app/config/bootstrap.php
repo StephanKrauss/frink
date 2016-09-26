bbd585a2e52611ff153ed957a1aa61fa21bc41b3
Stephan Krauss <develop@stephankrauss.de>
2016-09-26 10:18:17 +0200

    anlegen Cache für Twig

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -184,13 +184,17 @@ function startTwig()
     $loader = new Twig_Loader_Filesystem($viewPath);
 
     $twigConfig = array(
-        // 'cache' => './cache/twig/',
+        'cache' => './cache/',
         // 'cache' => false,
         'debug' => true,
     );
 
     Flight::register('view', 'Twig_Environment', array($loader, $twigConfig), function ($twig) {
-        $twig->addExtension(new Twig_Extension_Debug()); // Add the debug extension
+        // Add the Debug extension
+        $twig->addExtension(new Twig_Extension_Debug());
+
+        // Add the translation
+        // $twig->
     });
 
     return;
